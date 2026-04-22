5083e65492fcf76fe57f4bc94605eaac3f6e2fcd
popnegro <grasso.luis@gmail.com>
2026-04-22 09:00:38 -0300

    Commit

--- api/config.js
+++ api/config.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   res.status(200).json({
-    mapsKey: process.env.GOOGLE_MAPS_API_KEY || "AIzaSyAmTzHBhtQ4E8BG6DL0xdrEOYdZmQEBXkI",
+    mapsKey: process.env.GOOGLE_MAPS_API_KEY || "",
     whatsappNumber: process.env.WHATSAPP_NUMBER || "5492613871088",
     brandColor: "#fbbf24"
   });
